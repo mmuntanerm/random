92b55aca5df19c855d2a1de2f249aab86c1fd182
Miquel A. Muntaner Morey <m.muntaner.uoc@gmail.com>
2016-03-20 20:39:58 +0100

    rama 1 :  Contaje del acumulado de las veces que sale mayor o memor que 0.5

--- aleat.js
+++ aleat.js
@@ -6,6 +6,9 @@
 
 // fase 1
 var veces =10;
+var mayor =0;
+var menor =0;
+
 console.log("#################################################################\n");
 console.log("Genera numeros aleatorios y los compara con 0.5 \n");
 console.log("Repite el proceso " + veces + " veces \n");
@@ -21,11 +24,17 @@ for (var i = 0; i < veces; i++) {
 		if (numero <= 0.5)
 		{
 			console.log("MENOR QUE 0.5");
+			menor++;
 		}
 		else
 		{
 			console.log("MAYOR  que 0.5");
+			mayor++;
 		}
 
 }
+console.log(" ------------------------------------------------ \n");
+console.log("Los numero aleatorios han salido " + mayor + " veces mayor que 0.5");
+console.log("Los numero aleatorios han salido " + menor + " veces MENOR que 0.5");
+console.log(" ------------------------------------------------ \n");
 console.log("\n");
